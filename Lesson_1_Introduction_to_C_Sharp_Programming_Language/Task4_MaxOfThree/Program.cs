22a5944c1bbc9c4194187a6fe3ed0e56e43b68f4
Anton <ak17961@gmail.com>
2023-09-13 23:56:28 +0300

    Added Examples

--- Lesson_1_Introduction_to_C_Sharp_Programming_Language/Task4_MaxOfThree/Program.cs
+++ Lesson_1_Introduction_to_C_Sharp_Programming_Language/Task4_MaxOfThree/Program.cs
@@ -1,21 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите a: ");
-int a = int.Parse(Console.ReadLine());
+/* 
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+ */
+ 
+int a = InputNumber("Введите a: ");
+int b = InputNumber("Введите b: ");
+int c = InputNumber("Введите c: ");
 
-Console.Write("Введите b: ");
-int b = int.Parse(Console.ReadLine());
+int max = FindMax(a, b, c);
 
-Console.Write("Введите c: ");
-int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Максимальное число: " + max);
 
-int max;
-
-if (a >= b && a >= c) {
-  max = a;
-} else if (b >= a && b >= c) {
-  max = b;
-} else {
-  max = c;
+static int InputNumber(string message)
+{
+  Console.Write(message);
+  return int.Parse(Console.ReadLine());
 }
 
-Console.WriteLine("Максимальное число:" + max);
+static int FindMax(int a, int b, int c)
+{
+  int max = a;
+  if (b > max)
+  {
+    max = b;
+  }
+  if (c > max)
+  {
+    max = c;
+  }
+  return max;
+}
--- Lesson_1_Introduction_to_C_Sharp_Programming_Language/Task8_EvenNumbers/Program.cs
+++ Lesson_1_Introduction_to_C_Sharp_Programming_Language/Task8_EvenNumbers/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/* 
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+ */
 Console.Write("Введите число (N): ");
 int N = int.Parse(Console.ReadLine());
 
