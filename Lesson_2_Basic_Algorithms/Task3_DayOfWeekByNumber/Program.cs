130c84674649846c8286cb7a3fcc61fe2f785b51
Anton <ak17961@gmail.com>
2023-09-25 16:25:48 +0300

    modified:   Lesson_2_Basic_Algorithms/README.md

---
+++ Lesson_2_Basic_Algorithms/Task3_DayOfWeekByNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿/* 
+Задача 3:
+    Напишите программу, которая будет выдавать название
+    дня недели по заданному номеру.
+    
+    Пример:
+    5 -> Среда
+    3 -> Пятница
+ */
+
+Console.Clear();
+
+string[] daysOfWeek = { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
+
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number >= 1 && number <= 7)
+{
+    string dayOfWeek = daysOfWeek[number - 1];
+    Console.WriteLine($"День недели под номером {number} - это {dayOfWeek}");
+}
+else
+{
+    Console.WriteLine("Некорректный ввод. Введите целое число от 1 до 7.");
+}
